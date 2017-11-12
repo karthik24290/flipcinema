de1802e221169dd5e72c229debafa168c5b67aca
karthik24290 <31400783+karthik24290@users.noreply.github.com>
2017-11-12 22:58:12 +0530

    Updates

---
+++ details.php
@@ -0,0 +1,182 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="/favicon.ico">
+
+        <title>FlipCinema</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="/dist/css/home.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <!-- Navbar-->
+        <div class="collapse bg-dark" id="navbarHeader">
+            <div class="container">
+                <div class="nav-subnav row">
+                    <ul class="list-unstyled nav-subnav__list ac">
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Short Films</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Movie Teasers</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Movie Trailers</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Folk Songs</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Poems</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Movie Trailers</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- navbar-->
+
+        <!-- Header-->
+        <div class="navbar navbar-dark bg-dark">
+            <div class="container d-flex justify-content-between">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="logo-ttl" href="#">Flip cinema</a>
+                <div class="nav-ttl">Browse</div>
+            </div>
+        </div>
+        <!-- Header-->
+
+
+        <!-- details-->
+        <div class="detail-wrap row">
+            <div class="col-sm-8">
+                <div class="detail-wrap__full">
+                    <div class="detail-wrap__content">
+                        <div class="detail-wrap__img">
+                            <img src="/dist/images/img.jpg">
+                            <div class="detail-wrap__duration">
+                                <img src="/dist/icons/play-button.svg">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="detail-wrap__wrap">
+                        <div class="detail-wrap__ttl">12AM New Tamil Short Film (with Eng subtitles)</div>
+                        <div class="detail-wrap__subttl">tamil cinema review</div>
+                        <div class="detail-wrap__iconwrap">
+                            <div class="detail-wrap__left">
+                                <i class="detail-wrap__icon svg-icon"><img src="dist/icons/like.svg"></i>
+                                <span class="detail-wrap__count">20k</span>
+                                <i class="detail-wrap__icon svg-icon"><img src="dist/icons/dislike.svg"></i>
+                                <span class="detail-wrap__count">4k</span>
+                            </div>
+                            <div class="detail-wrap__right">
+                                <i class="detail-wrap__ryticon svg-icon"><img src="dist/icons/share.svg"></i>
+                                <i class="detail-wrap__ryticon svg-icon"><img src="dist/icons/chat.svg"></i>
+                                <i class="detail-wrap__ryticon svg-icon"><img src="dist/icons/later.svg"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="detail-list__author">
+                        <div class="detail-list__desc">Watch Mazhai Varum Official Song Video from the Movie Veppam</div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="detail-list">
+                    <div class="detail-list__ttl">You May Also Like to Watch</div>
+                    <ul class="detail-list__list">
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__fig">
+                                        <img src="https://i.ytimg.com/vi/JCLGB0k5zzQ/mqdefault.jpg">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__fig">
+                                        <img src="https://i.ytimg.com/vi/6zwrmD-Bbrk/mqdefault.jpg">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__fig">
+                                        <img src="https://i.ytimg.com/vi/JpmYuSJhlYY/mqdefault.jpg">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__fig">
+                                        <img src="https://i.ytimg.com/vi/PBMRi84-eKM/mqdefault.jpg">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__fig">
+                                        <img src="https://i.ytimg.com/vi/X8cQHG04j6Y/mqdefault.jpg">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+        <!-- details-->
+
+
+
+        <!-- Footer-->
+        <footer class="footer-sec">
+            <div class="container">
+                <p class="float-right">
+                    <!--<a href="#">Back to top</a>-->
+                </p>
+                <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+                <!--<p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>-->
+            </div>
+        </footer>
+        <!-- Footer-->
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="/bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+        <!--<script src="/bower_components/popper.js/dist/popper.min.js"></script>-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/umd/popper.min.js" type="text/javascript"></script>
+        <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,222 @@
-<h1>It Works</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="/favicon.ico">
+
+        <title>FlipCinema</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="/dist/css/home.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <!-- Navbar-->
+        <div class="collapse bg-dark" id="navbarHeader">
+            <div class="container">
+                <div class="nav-subnav row">
+                    <ul class="list-unstyled nav-subnav__list ac">
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Short Films</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Movie Teasers</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Movie Trailers</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Folk Songs</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Poems</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Movie Trailers</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- Header-->
+
+        <!-- Header-->
+        <div class="navbar navbar-dark bg-dark">
+            <div class="container d-flex justify-content-between">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="logo-ttl" href="#">Flip cinema</a>
+                <div class="nav-ttl">Browse</div>
+            </div>
+        </div>
+        <!-- Header-->
+
+        <!-- banner-->
+        <div class="banner">
+            <div class="banner__bg">
+                <video autoplay="" loop="" muted="" id="awards-bg-video">
+                    <source src="/dist/banner.mp4" type="video/mp4">
+                    <source src="https://auto.ndtvimg.com/media/video/cnb-awards/awards-bg.webm" type="video/webm">        
+                </video>
+            </div>
+            <div class="jumbotron text-center">
+                <div class="container">
+                    <div class="banner__img">
+                        <h1 class="banner-ttl">Movie<span class="banner-subttl">pass</span></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Banner-->
+
+        <!-- Listing-->
+        <div class="album text-muted">
+            <div class="container">
+                <div class="card-container">
+                    <div class="row">
+                        <div class="card-ttl ac">Popular Shortfilms</div>
+                        <div class="col-sm-3 py-3">
+                            <a href="/details.php">
+                                <img class="card-img" src="https://i.ytimg.com/vi/xNP3n-VAsuk/hqdefault.jpg?sqp=-oaymwEWCNIBEHZIWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLBWaELWc6NAJry7ZTetNoHcb6vURw" alt="Card image cap">
+                                <p class="card-text">New theme for New Generation</p>
+                                <div class="card-wrap">
+                                    <span class="card-count">2K Views</span>
+                                    <span class="card-day">2 Days ago</span>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-3 py-3">
+                            <a href="/details.php">
+                                <img class="card-img" src="https://i.ytimg.com/vi/jCafZht8yMA/hqdefault.jpg?sqp=-oaymwEWCNIBEHZIWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLAQKBrp1o6Bh9ZHOznDsQdNHukZcA" alt="Card image cap">
+                                <p class="card-text">New theme for New Generation</p>
+                                <div class="card-wrap">
+                                    <span class="card-count">2K Views</span>
+                                    <span class="card-day">2 Days ago</span>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-3 py-3">
+                            <a href="/details.php">
+                                <img class="card-img" src="https://i.ytimg.com/vi/wK7shf-5soU/hqdefault.jpg?sqp=-oaymwEWCNIBEHZIWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLCTT42KZVlCJpd52IgpF8-H_vnzWA" alt="Card image cap">
+                                <p class="card-text">New theme for New Generation</p>
+                                <div class="card-wrap">
+                                    <span class="card-count">2K Views</span>
+                                    <span class="card-day">2 Days ago</span>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-sm-3 py-3">
+                            <a href="/details.php">
+                                <img  class="card-img" src="https://i.ytimg.com/vi/X0JXXLdDnp4/hqdefault.jpg?sqp=-oaymwEWCNIBEHZIWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLA4grozUqZGRQ9yF5FDm5DNHjDvgA" alt="Card image cap">
+                                <p class="card-text">New theme for New Generation</p>
+                                <div class="card-wrap">
+                                    <span class="card-count">2K Views</span>
+                                    <span class="card-day">2 Days ago</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-container">
+                    <div class="row">
+                        <div class="card-ttl ac">Trailers</div>
+
+                        <div class="col-sm-3 py-3">
+                            <a href="/details.php">
+                                <img class="card-img" src="https://i.ytimg.com/vi/fhZk4GzC1QY/hqdefault.jpg?sqp=-oaymwEWCNIBEHZIWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLBFqBkKBXYgMQJGaVEGJ1VVHt9RLg" alt="Card image cap">
+                                <p class="card-text">New theme for New Generation</p>
+                                <div class="card-wrap">
+                                    <span class="card-count">2K Views</span>
+                                    <span class="card-day">2 Days ago</span>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-3 py-3">
+                            <a href="/details.php">
+                                <img class="card-img" src="https://i.ytimg.com/vi/KV8Wpnpm_lg/hqdefault.jpg?sqp=-oaymwEWCNIBEHZIWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLCbiAg7455K3G4LB00hQDuq3ZDU7Q" alt="Card image cap">
+                                <p class="card-text">New theme for New Generation</p>
+                                <div class="card-wrap">
+                                    <span class="card-count">2K Views</span>
+                                    <span class="card-day">2 days ago</span>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-3 py-3">
+                            <a href="/details.php">
+                                <img class="card-img" src="https://i.ytimg.com/vi/UYnBb0bDPSM/hqdefault.jpg?sqp=-oaymwEWCNIBEHZIWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLBDTsl3-v5ye1X050ZtrEdqR1zKAA" alt="Card image cap">
+                                <p class="card-text">New theme for New Generation</p>
+                                <div class="card-wrap">
+                                    <span class="card-count">2K Views</span>
+                                    <span class="card-day">2 Days ago</span>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="col-sm-3 py-3">
+                            <a href="/details.php">
+                                <img class="card-img" src="https://i.ytimg.com/vi/HQXaZewuc8o/hqdefault.jpg?sqp=-oaymwEWCNIBEHZIWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLA1RHRi2grWxxPJ5DjwDuQzjtQcAg" alt="Card image cap">
+                                <p class="card-text">New theme for New Generation</p>
+                                <div class="card-wrap">
+                                    <span class="card-count">2K Views</span>
+                                    <span class="card-day">2 Days ago</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="ac">
+                    <button class="more-btn">View More Categories</button>
+                </div>
+            </div>
+        </div>
+        <!-- Listing-->
+
+
+        <!-- description-->
+        <div class="">
+            <div class="extend-content">
+                <div class="desc-sec">
+                    <ul class="desc-sec__list row">
+                        <li class="col-sm-4 desc-sec__item ac">
+                            <img class="desc-sec__img" src="dist/images/icons/play.svg">
+                            <div class="desc-sec__text">Watch Millions of <br>videos online</div>
+                        </li>
+                        <li class="col-sm-4 desc-sec__item ac">
+                            <img class="desc-sec__img" src="dist/images/icons/upload.svg">
+                            <div class="desc-sec__text">Upload and Manage <br>your videos online</div>
+                        </li>
+                        <li class="col-sm-4 desc-sec__item ac">
+                            <img class="desc-sec__img" src="dist/images/icons/device.svg">
+                            <div class="desc-sec__text">Watch anywhere, <br>on any device</div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- description-->
+
+        <!--invite-sec-->
+        <div class="bg-white container">
+            <div class="invite-sec ac">
+                <div class="invite-sec__banner"></div>
+                <div class="invite-sec__ttl">Join FlipCinema</div>
+                <div class="invite-sec__txt">In this special occassion join and enjoy over tons of music <br> experience in all gener and enjoy.</div>
+                <a href="#" class="btn invite-sec__btn">Join Now</a>
+            </div>
+        </div>
+        <!--invite-sec-->
+
+        <!-- Footer-->
+        <footer class="footer-sec">
+            <div class="container">
+                <p class="float-right">
+                    <!--<a href="#">Back to top</a>-->
+                </p>
+                <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+                <!--<p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>-->
+            </div>
+        </footer>
+        <!-- Footer-->
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="/bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+        <!--<script src="/bower_components/popper.js/dist/popper.min.js"></script>-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/umd/popper.min.js" type="text/javascript"></script>
+        <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
