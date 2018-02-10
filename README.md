b9b8c25b032a17a357d6ccb4b1a1a5ba1c60198c
karthik24290 <31400783+karthik24290@users.noreply.github.com>
2018-02-10 23:17:21 +0530

    bower components push

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # flipcinema
-mama
+
+Home page
+- Menu
+
+Details page
+ - Login scenario
+
+Upload
+Registration
+About Us
+Contact us
+
+Bootstrap Ref:
+https://getbootstrap.com/docs/4.0/components/forms/
---
+++ listing.php
@@ -0,0 +1,164 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="/favicon.ico">
+
+        <title>FlipCinema</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="/dist/css/home.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <!-- Navbar-->
+        <div class="collapse bg-dark" id="navbarHeader">
+            <div class="container">
+                <div class="nav-subnav row">
+                    <ul class="list-unstyled nav-subnav__list ac">
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Short Films</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Movie Teasers</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Movie Trailers</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Folk Songs</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Poems</a></li>
+                        <li class="nav-subnav__item"><a href="#" class="nav-subnav__link text-white">Movie Trailers</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- navbar-->
+
+        <!-- Header-->
+        <div class="navbar navbar-dark bg-dark">
+            <div class="container d-flex justify-content-between">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="logo-ttl" href="index.php">Flip cinema</a>
+                <div class="nav-ttl">Browse</div>
+            </div>
+        </div>
+        <!-- Header-->
+
+
+        <!-- details-->
+        <div class="detail-wrap row">
+
+            <div class="col-sm-10">
+                <div class="detail-list">
+                    <div class="detail-list__ttl">You May Also Like to Watch</div>
+                    <ul class="detail-list__list">
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__img">
+                                        <img src="https://i.ytimg.com/vi/aBoKCe7GHIk/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA3xRzeI46hyF7AqsleywFm5HiiQg">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__img">
+                                        <img src="https://i.ytimg.com/vi/qbx1dCI8tco/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDgeeZ-s1__rLndb107nTEQfLTalQ">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__img">
+                                        <img src="https://i.ytimg.com/vi/aBoKCe7GHIk/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA3xRzeI46hyF7AqsleywFm5HiiQg">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__img">
+                                        <img src="https://i.ytimg.com/vi/qbx1dCI8tco/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDgeeZ-s1__rLndb107nTEQfLTalQ">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__img">
+                                        <img src="https://i.ytimg.com/vi/aBoKCe7GHIk/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA3xRzeI46hyF7AqsleywFm5HiiQg">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="detail-list__item">
+                            <a class="detail-list__link">
+                                <div class="detail-list__wrap">
+                                    <div class="detail-list__img">
+                                        <img src="https://i.ytimg.com/vi/qbx1dCI8tco/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDgeeZ-s1__rLndb107nTEQfLTalQ">
+                                    </div>
+                                    <div class="detail-list__content">
+                                        <div class="detail-list__subttl">Happy New Year - New Tamil Short Film 2017 || Vijjith, Akshara Sudhakar Reddy || E Sub</div>
+                                        <div class="detail-list__name">Tamil Cinema Review</div>
+                                        <div class="detail-list__views">1,546,434 views</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+        <!-- details-->
+
+
+
+        <!-- Footer-->
+        <footer class="footer-sec">
+            <div class="container">
+                <p class="float-right">
+                    <!--<a href="#">Back to top</a>-->
+                </p>
+                <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+                <!--<p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>-->
+            </div>
+        </footer>
+        <!-- Footer-->
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="/bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+        <!--<script src="/bower_components/popper.js/dist/popper.min.js"></script>-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/umd/popper.min.js" type="text/javascript"></script>
+        <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
